0eef8cdf41b07322ef934b1f0cc833ef820e6d6f
sharpie-stories <getg93@gmail.com>
2025-10-25 19:04:01 -0600

    fix: Consolidate Deno config and type Edge Function request

--- supabase/functions/translate-and-store/index.ts
+++ supabase/functions/translate-and-store/index.ts
@@ -13,7 +13,7 @@ const corsHeaders = {
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
 };
 
-serve(async (req) => {
+serve(async (req: Request) => {
   if (req.method === 'OPTIONS') {
     return new Response('ok', { headers: corsHeaders });
   }
